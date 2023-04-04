3fb3c0baa6649e613d818edbfc43850acd3e9d57
David Dume <dumedavid11@gmail.com>
2023-04-04 14:01:05 +0200

    creazione di un modal

---
+++ src/components/Modal.vue
@@ -0,0 +1,54 @@
+<template>
+
+    <div class="modal-container">
+
+       <div class="modal">
+        <div class="btn">
+            <button @click="$emit('closeModal')">X</button>
+        </div>
+        {{ title }}
+       </div>
+
+    </div>
+
+
+</template>
+
+<script>
+    export default {
+        name: 'Modal',
+        props: {
+            title: String
+        }
+    }
+</script>
+
+
+<style lang="scss" scoped>
+    .modal-container {
+        position: fixed;
+        top: 0;
+        left: 0;
+        z-index: 9998;
+        width: 100%;
+        height: 100%;
+        background: rgba(64, 64, 64, 0.36);
+        border-radius: 16px;
+        box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+        backdrop-filter: blur(3.3px);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        .modal {
+            width: 600px;
+            height: 700px;
+            background-color: white;
+            border: 1px solid black;
+            .btn {
+                text-align: right;
+                padding: 10px;
+            }
+        }
+    }
+
+</style>
--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,6 +1,15 @@
 <template>
+    <div class="modal">
+        <Modal v-if="showModal" @closeModal="showModal = false" 
+        :title="modalInfo.original_title"
+        
+        ></Modal>
+    </div>
+
     <div class="card-list" >
-        <div class="card" v-for="movie in store.movieList">
+        
+        <div class="card" v-for="(movie, index) in store.movieList" :key="index" @click="getModal(index)">
+        
             <MovieCard 
             :title="movie.title"
             :original_title="movie.original_title"
@@ -10,9 +19,9 @@
             ></MovieCard>
         </div>
     </div>
-    <h2>TV Series</h2>
+    <h2 v-if="store.tvList.length > 0">TV Series</h2>
     <div class="card-list">
-        <div class="card" v-for="tv in store.tvList">
+        <div class="card" v-for="(tv, index) in store.tvList" :key="index" @click="showModal = true">
             <MovieCard 
             :title="tv.title"
             :original_title="tv.original_name"
@@ -20,22 +29,27 @@
             :score="getScore(tv.vote_average)"
             :img="getImg(tv.poster_path)"
             ></MovieCard>
+           
         </div>
     </div>
 </template>
 
 <script>
     import MovieCard from './MovieCard.vue';
+    import Modal from './Modal.vue';
     import { store } from '../store.js';
 
     export default {
         name: 'MovieList',
         components: {
-            MovieCard
+            MovieCard,
+            Modal
         },
         data() {
             return {
-                store
+                store,
+                showModal: false,
+                modalInfo: {}
             }
         },
         methods: {
@@ -46,12 +60,17 @@
                 score = Math.ceil(score);
                 let percentage = score * 10;
                 return Math.ceil((5/100)*percentage);
+            },
+            getModal(index) {
+                this.showModal = true;
+                this.modalInfo = this.store.movieList[index];
             }
         }
     }
 </script>
 
 <style lang="scss">
+
 
     .card-list {
         display: flex;
